6f8e91840cbae25f749532b6684626f20849b944
Davidmide Adenusi <Davidmide07@gmail.com>
2023-05-12 20:51:37 +0100

    animation going

--- index.js
+++ index.js
@@ -1,59 +1,56 @@
-const header = document.querySelector(".header");
-
-function animeFn() {
-    header.classList.add("animateH");
-    // alert('scroll is scrollable')
-    // console.log('hellow');
-}
-
-header.addEventListener("click", animeFn);
-
-
 const nameD = document.getElementById("name");
 const write = new Typewriter(nameD, {
   loop: false,
-  delay: 50,
+  delay: 20,
 });
 
-write
-  .typeString("I'm Davimide.")
-  .pauseFor(300)
-  .typeString("  <p>Welcome to my portfolio site ❤👌</p>")
-  .pauseFor(300)
-  .deleteChars(32 )
-  .typeString(
-    "<p>A <span style='color:rgb(212, 230, 20)' >critical Thinker</span> and <span style='color:rgb(212, 230, 20)' >problem Solver</sapn>,</p>"
-  )
-  .pauseFor(300)
-  .typeString('who uses technology to build')
-  .pauseFor(300)
-  .typeString(' <span >and create values that makes life easier with the zeal to learn as</span>')
-  .pauseFor(300)
-  .typeString(' technology keeps evolving.')
-  .start();
-
+write.typeString("I'm Davimide.").start();
 
 //   another typewriter effect
-// let para = document.getElementById("para");
+let para = document.getElementById("para");
 
-// var typewriter = new Typewriter(para, {
-//   loop: true,
-//   delay: 75,❤
+var typewriter = new Typewriter(para, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(300)
+  .typeString(
+    "  <p style='color:rgb(212, 230, 20)'>Welcome to my portfolio site ❤👌</p>"
+  )
+  .deleteAll()
+  .pauseFor(300)
+  .typeString(
+    "<p>A <span style='color:rgb(212, 230, 20)' >Critical Thinker</span> and <span style='color:rgb(212, 230, 20)' >Problem Solver</sapn>,</p>"
+  )
+  .pauseFor(300)
+  .typeString("who uses technology to build")
+  .pauseFor(300)
+  .typeString(
+    " <span >and create values that make life easier with the zeal to <span style='color:rgb(212, 230, 20)'> learn, relearn and unlearn as</span>"
+  )
+  .pauseFor(300)
+  .typeString(" technology keeps evolving.")
+  .pauseFor(300)
+  .deleteAll(10)
+  .start();
+
+// document.querySelectorAll("ul li").forEach((btn, index) => {
+//   btn.addEventListener("click", () => {
+//     gsap.to(window, {
+//       duration: 1,
+//       scrollTo: { y: "#section" + (index + 1), offsetY: 70 },
+//     });
+//   });
 // });
+gsap.registerPlugin(ScrollTrigger);
+gsap.to( ".animate-area",{
 
-// typewriter
-//   .pauseFor(5500)
-//   .typeString(
-//     "<p>A <span style='color:rgb(212, 230, 20)' >critical Thinker</span> and <span style='color:rgb(212, 230, 20)' >problem Solver</sapn>,</p>"
-//   )
-//   .pauseFor(300)
-//   .typeString(" who uses technology to build")
-//   .typeString(
-//     " <span >and create values that makes life easier with the zeal to learn as</span>"
-//   )
-//   .typeString(" technology keeps evolving.")
-//   .pauseFor(1000)
-//   .start();
+  x:100,
+  // rotation:27,
+  opacity:0,
+  duration:2,
+}
 
-
-
+)
